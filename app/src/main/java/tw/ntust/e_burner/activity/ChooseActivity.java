8bd9acc699534b12644c265a4848599d688df3ca
張霽 <edingroot@gmail.com>
2015-12-28 01:21:59 +0800

    Layout choose_amount (TODO: Styling listAmoun).

--- app/src/main/java/tw/ntust/e_burner/activity/ChooseActivity.java
+++ app/src/main/java/tw/ntust/e_burner/activity/ChooseActivity.java
@@ -3,13 +3,17 @@ package tw.ntust.e_burner.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import tw.ntust.e_burner.R;
 
 public class ChooseActivity extends Activity {
 
+    // -- layout.choose_target
     private ImageView imgEmperor, imgAncestor;
+    // -- layout.choose_amount
+    private Button btnStart, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +23,31 @@ public class ChooseActivity extends Activity {
     }
 
     private void initComponents_chooseTarget() {
-        imgEmperor = (ImageView) findViewById(R.id.chooseAmount_imgEmperor);
-        imgAncestor = (ImageView) findViewById(R.id.chooseAmount_imgAncestor);
+        imgEmperor = (ImageView) findViewById(R.id.chooseTarget_imgEmperor);
+        imgAncestor = (ImageView) findViewById(R.id.chooseTarget_imgAncestor);
 
         View.OnClickListener onChooseTargetClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                setContentView(R.layout.choose_amount);
+                initComponents_chooseAmount();
             }
         };
         imgEmperor.setOnClickListener(onChooseTargetClickListener);
         imgAncestor.setOnClickListener(onChooseTargetClickListener);
+    }
+
+    private void initComponents_chooseAmount() {
+        btnStart = (Button) findViewById(R.id.chooseAmount_btnStart);
+        btnCancel = (Button) findViewById(R.id.chooseAmount_btnCancel);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.choose_target);
+                initComponents_chooseTarget();
+            }
+        });
     }
 
 }
