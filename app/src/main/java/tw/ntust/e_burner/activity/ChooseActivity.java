3178bcb98dcbb95e6a84be9d8d414703fe40d27c
張霽 <edingroot@gmail.com>
2015-12-27 09:17:34 +0800

    Set activities screen orientation to portrait. modify layout activity_main. Add ChooseActivity as main fragment parent. Add icons.

---
+++ app/src/main/java/tw/ntust/e_burner/activity/ChooseActivity.java
@@ -0,0 +1,15 @@
+package tw.ntust.e_burner.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import tw.ntust.e_burner.R;
+
+public class ChooseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+    }
+}
--- app/src/main/java/tw/ntust/e_burner/activity/MainActivity.java
+++ app/src/main/java/tw/ntust/e_burner/activity/MainActivity.java
@@ -3,18 +3,36 @@ package tw.ntust.e_burner.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import tw.ntust.e_burner.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
 
         // ---------- FOR DEBUG ----------
         // startActivity(new Intent(MainActivity.this, SelectDevice.class));
         // -------------------------------
     }
+
+    private void initComponents() {
+        btnLogin = (Button) findViewById(R.id.main_btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ChooseActivity.class));
+                finish();
+            }
+        });
+    }
+
 }
