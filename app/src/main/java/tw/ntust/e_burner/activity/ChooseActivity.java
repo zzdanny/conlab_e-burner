8f0677bf44920e72d9aad3ad3fabaa47a86a0e78
張霽 <edingroot@gmail.com>
2015-12-28 01:38:35 +0800

    Add startup progress bar animation in ChooseActivity.

--- app/src/main/java/tw/ntust/e_burner/activity/ChooseActivity.java
+++ app/src/main/java/tw/ntust/e_burner/activity/ChooseActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import tw.ntust.e_burner.R;
 
 public class ChooseActivity extends Activity {
 
+    // -- layout.activity_choose
+    private ProgressBar progressEmperor, progressAncestor;
     // -- layout.choose_target
     private ImageView imgEmperor, imgAncestor;
     // -- layout.choose_amount
@@ -20,9 +23,19 @@ public class ChooseActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_target);
         initComponents_chooseTarget();
+        
+        animateProgressBar(progressAncestor, 50);
+        animateProgressBar(progressEmperor, 50);
+    }
+
+    private void initComponents_choose() {
+        progressAncestor = (ProgressBar) findViewById(R.id.choose_progressAncestor);
+        progressEmperor = (ProgressBar) findViewById(R.id.choose_progressEmperor);
     }
 
     private void initComponents_chooseTarget() {
+        initComponents_choose();
+
         imgEmperor = (ImageView) findViewById(R.id.chooseTarget_imgEmperor);
         imgAncestor = (ImageView) findViewById(R.id.chooseTarget_imgAncestor);
 
@@ -38,6 +51,8 @@ public class ChooseActivity extends Activity {
     }
 
     private void initComponents_chooseAmount() {
+        initComponents_choose();
+
         btnStart = (Button) findViewById(R.id.chooseAmount_btnStart);
         btnCancel = (Button) findViewById(R.id.chooseAmount_btnCancel);
 
@@ -48,6 +63,28 @@ public class ChooseActivity extends Activity {
                 initComponents_chooseTarget();
             }
         });
+    }
+
+    private void animateProgressBar(final ProgressBar progressBar, final int value) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= value; i++) {
+                    final int num = i;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(num);
+                        }
+                    });
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
 }
